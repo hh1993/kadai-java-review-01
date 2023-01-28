1dd65bb25c95206869425d2679b13700780594b2
hh1993 <h.hiroyoshi1227@gmail.com>
2023-01-28 16:20:50 +0900

    ｔａｘメソッドの名前変更。floorメソッドの削除

--- src/Review01.java
+++ src/Review01.java
@@ -3,17 +3,16 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price   = 1443; //商品価格を代入
-        int taxRate = 10/100;   //税率を代入
+        int taxRate = 10;   //税率を代入
         int VAT;
-        VAT = taxMethod(price, taxRate);
-        System.out.println(price + "円の商品の税込価格は" + Math.floor(price + VAT) + "円（消費税は" + Math.floor(VAT) + "円）です。");
+        VAT = tax(price, taxRate);
+        System.out.println(price + "円の商品の税込価格は" + (price + VAT) + "円（消費税は" + (VAT) + "円）です。");
     }
 
-    public static int taxMethod(int price, int taxRate) {
-        int VAT = price * taxRate; //消費税額を計算
+    public static int tax(int price, int taxRate) {
+        int VAT = price * taxRate/100; //消費税額を計算
         return VAT;
 
     }
 
 }
-
